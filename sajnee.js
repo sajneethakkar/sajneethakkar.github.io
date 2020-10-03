fcdcf27ddd032bfec7c11e620188b18a815ddbc9
sajneethakkar <curioust6@gmail.com>
2020-10-03 05:52:02 -0700

    portfolio revamp - october 2020
    added new home inverse picture, updated content for experiences and projects pages, considering changing volunteering altogether

---
+++ sajnee.js
@@ -0,0 +1,9 @@
+function inverse() {
+  document.getElementById("image").src = 'picture2.jpg';
+  document.getElementById("name").style.color = '#3C3D00';
+}
+
+function original() {
+  document.getElementById("image").src = 'picture.jpg';
+  document.getElementById("name").style.color = '#BF391B';
+}
